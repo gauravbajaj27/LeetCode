317e8b6aa7ebed577fa1b44b3d5ce016138c2634
Gaurav Bajaj <60040126+gauravbajaj27@users.noreply.github.com>
2022-07-24 03:29:26 +0530

    Time: 1628 ms (6.00%), Space: 144.1 MB (12.16%) - LeetHub

--- 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.java
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.java
@@ -1,32 +1,41 @@
 class Solution {
-    public List<Integer> countSmaller(int[] nums) {
-        LinkedList<Integer> result = new LinkedList<>();
-        List<Integer> list = new ArrayList<>();
-        for(int i = nums.length-1; i >= 0; i--) {
-            int idx = binarySearch(list, nums[i]);
-            result.addFirst(idx);
-            list.add(idx, nums[i]);
+    public List<Integer> countSmaller(int[] arr) {
+        //Binary Search Approach
+        // Create a List ans to store ans of each index
+        //Start from the extreme left of nums arr and put each element in the second list on a specified index returned by binary search performing on second list (al2);
+        
+        
+        LinkedList<Integer> ans= new LinkedList<>();
+        List<Integer> al2= new ArrayList<>();
+        
+        int n=arr.length;
+        
+        
+        for(int i=n-1;i>=0;i--){
+            int index=binarySearch(al2,arr[i]);
+            ans.addFirst(index);
+            al2.add(index,arr[i]);
         }
-        return result;
+        return ans;    
+        
     }
     
-	//Returns index of the first number that is equal to or bigger than num
-    public int binarySearch(List<Integer> list, int num) {
-        int left = 0;
-        int right = list.size()-1;
-        int mid;
-        while(left <= right) {
-            mid = left + (right-left)/2;
-            if(list.get(mid) < num) {
-                left = mid+1;
-            }
-            else {
-                right = mid-1;
-            }
-        }
-        // System.out.println(left);
-        // System.out.println(list);
+    public int binarySearch(List<Integer> al, int num){
         
+        int left=0;
+        int right=al.size()-1;
+        int mid=0;
+        
+        while(left<=right){
+            mid=(right+left)/2;
+            if(al.get(mid)<num){
+                left=mid+1;
+            }
+            else right=mid-1;
+        }
         return left;
+        
+        
     }
+    
 }
