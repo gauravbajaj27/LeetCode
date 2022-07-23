15ce42365130acfb8ec33d6f78751481e55607eb
Gaurav Bajaj <60040126+gauravbajaj27@users.noreply.github.com>
2022-07-24 03:30:20 +0530

    Time: 1057 ms (10.83%), Space: 140.4 MB (28.71%) - LeetHub

--- 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.java
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.java
@@ -3,39 +3,26 @@ class Solution {
         //Binary Search Approach
         // Create a List ans to store ans of each index
         //Start from the extreme left of nums arr and put each element in the second list on a specified index returned by binary search performing on second list (al2);
-        
-        
         LinkedList<Integer> ans= new LinkedList<>();
         List<Integer> al2= new ArrayList<>();
-        
         int n=arr.length;
-        
-        
         for(int i=n-1;i>=0;i--){
             int index=binarySearch(al2,arr[i]);
             ans.addFirst(index);
             al2.add(index,arr[i]);
         }
-        return ans;    
-        
+        return ans;
     }
-    
     public int binarySearch(List<Integer> al, int num){
-        
         int left=0;
         int right=al.size()-1;
         int mid=0;
-        
         while(left<=right){
             mid=(right+left)/2;
-            if(al.get(mid)<num){
-                left=mid+1;
-            }
+            if(al.get(mid)<num) left=mid+1;
             else right=mid-1;
         }
         return left;
-        
-        
     }
     
 }
